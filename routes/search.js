a8a72464d7017c5849fc9463835958192d227b1f
Kiet Vo <geforce2409@gmail.com>
2018-12-23 01:28:21 +0700

    init question model

--- routes/search.js
+++ routes/search.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('search');
+    res.render('search', {title: 'Travelie - Search homestay'});
 });
 
 
